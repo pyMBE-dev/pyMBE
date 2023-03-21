87e5ae97522cdb83c9335e6974feae4489fc0c90
Paola <paotorres89@gmail.com>
2023-03-21 16:30:33 -0300

    added test script for globular protein

---
+++ tests/1F6S_globular_protein.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import inspect
+
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
+# Create an instance of pyMBE library
+import pyMBE
+pmb = pyMBE.pymbe_library()
+
+# The trajectories of the simulations will be stored using espresso built-up functions in separed files in the folder 'frames'
+if not os.path.exists('./frames'):
+    os.makedirs('./frames')
+
+#Directory of the protein model 
+protein_filename = 'tests/coarse_grain_model_of_1f6s.vtf'
+#Reads the VTF file of the protein model
+pmb.read_protein_vtf_file(filename=protein_filename)
