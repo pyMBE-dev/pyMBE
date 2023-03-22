69e9742bae39488523a84507d11f17b41a68343c
Paola <paotorres89@gmail.com>
2023-03-22 17:09:26 -0300

    added functions on the sample script

--- sample_scripts/1F6S_globular_protein.py
+++ sample_scripts/1F6S_globular_protein.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import inspect
-
+import espressomd
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
@@ -15,9 +15,26 @@ pmb = pyMBE.pymbe_library()
 if not os.path.exists('./frames'):
     os.makedirs('./frames')
 
-#Directory of the protein model 
-protein_filename = 'tests/coarse_grain_model_of_1f6s.vtf'
-#Reads the VTF file of the protein model
-pmb.load_protein_vtf_file(filename=protein_filename)
 
-print(pmb.df.head(10))
+#System Parameters 
+
+c_salt    =  0.01  * pmb.units.mol / pmb.units.L  
+c_protein =  2e-4 * pmb.units.mol / pmb.units.L 
+Box_V =  1. / (pmb.N_A*c_protein)
+Box_L = Box_V**(1./3.) 
+
+espresso_system = espressomd.System(box_l=[Box_L.to('reduced_length').magnitude] * 3)
+
+
+#Directory of the protein model 
+protein_filename = 'sample_scripts/coarse_grain_model_of_1f6s.vtf'
+#Reads the VTF file of the protein model
+pdb_code = '1f6s'
+
+protein_positions = pmb.load_protein_vtf_in_df (name=pdb_code,filename=protein_filename)
+
+pmb.create_protein_in_espresso(name=pdb_code,
+                               number_of_proteins=1,
+                               espresso_system=espresso_system,
+                               positions=protein_positions)
+print(pmb.df.head(100))
