8c4dbef40c40c4c4ed5fc8be21fbc9267b5d2d08
Paola <paotorres89@gmail.com>
2023-04-24 21:11:03 -0300

    modify script to test several ph

--- sample_scripts/1F6S_globular_protein.py
+++ sample_scripts/1F6S_globular_protein.py
@@ -3,6 +3,7 @@ import sys
 import inspect
 from tqdm import tqdm
 import espressomd
+import argparse
 
 from espressomd import interactions
 from espressomd.io.writer import vtf
@@ -17,8 +18,6 @@ import pyMBE
 pmb = pyMBE.pymbe_library()
 
 from handy_scripts.handy_functions import  calculate_net_charge
-
-
 # Here you can adjust the width of the panda columns displayed when running the code 
 pmb.pd.options.display.max_colwidth = 10
 
@@ -26,8 +25,16 @@ pmb.pd.options.display.max_colwidth = 10
 if not os.path.exists('./frames'):
     os.makedirs('./frames')
 
+parser = argparse.ArgumentParser(description='Script to run globular protein simulation in espressomd')
+parser.add_argument('--pH', type=float,help='Expected pH value')
+
+args = parser.parse_args ()
+pH_value = args.pH 
+print (f"Running simulation for: {pH_value}")
+
+
 #System Parameters 
-pH_value = 2.0
+# pH_value = 2.0
 pmb.set_reduced_units(unit_length=0.4*pmb.units.nm)
 
 c_salt    =  0.01  * pmb.units.mol / pmb.units.L  
@@ -37,16 +44,23 @@ Box_L = Box_V**(1./3.)
 
 SEED = 77 
 solvent_permitivity = 78.3
-dt = 0.001
 
-Samples_per_pH = 500
-MD_steps_per_sample = 500
-steps_eq = int(Samples_per_pH/3)
-N_samples_print = 10  # Write the trajectory every 100 samples
+#Simulation Parameters
+t_max = 1e3 #  in LJ units of time
+stride_obs = 10 #  in LJ units of time
+stride_traj = 100 # in LJ units of time
+dt = 0.001
+N_samples = int (t_max / stride_obs)
+integ_steps = int (stride_obs/dt)
+
 probability_reaction = 0.5 
 
-bead_size = 0.4*pmb.units.nm
+bead_size = 0.355*pmb.units.nm
 epsilon = 1*pmb.units('reduced_energy')
+
+
+WCA = False
+Electrostatics = False
 
 espresso_system = espressomd.System(box_l=[Box_L.to('reduced_length').magnitude] * 3)
 espresso_system.virtual_sites = espressomd.virtual_sites.VirtualSitesRelative()
@@ -57,40 +71,37 @@ protein_filename = 'sample_scripts/coarse_grain_model_of_1f6s.vtf'
 
 #Reads the VTF file of the protein model
 protein_positions = pmb.load_protein_vtf_in_df (name=protein_name,filename=protein_filename)
+protein_sequence = pmb.df.loc[pmb.df['name']== protein_name].sequence.values[0]
+
+print (f'The protein sequence is: {protein_sequence}')
 
 pmb.define_particle(name='Ca',q=0,diameter=bead_size,epsilon=epsilon)
 
-
 # Solution 
-
 cation_name = 'Na'
 anion_name = 'Cl'
 pmb.define_particle(name=cation_name,  q=1, diameter=0.2*pmb.units.nm, epsilon=epsilon)
-pmb.define_particle(name=anion_name,  q=-1, diameter=0.36*pmb.units.nm,  epsilon=epsilon)
+pmb.define_particle(name=anion_name,  q=-1, diameter=0.2*pmb.units.nm,  epsilon=epsilon)
 
-#We define each aminoacid in the pyMBE data frame
+# Here we upload the pka set from the reference_parameters folder 
 
-pmb.load_pka_set (filename='reference_parameters/pka_sets/CRC1991.txt')
+pmb.load_pka_set (filename='reference_parameters/pka_sets/Nozaki1967.txt')
 
-
-
-protein_sequence = pmb.df.loc[pmb.df['name']== protein_name].sequence.values[0]
-
-
+#We create the protein in espresso 
 
 pmb.create_protein_in_espresso(name=protein_name,
-                               number_of_proteins=2,
+                               number_of_proteins=1,
                                espresso_system=espresso_system,
                                positions=protein_positions)
 
+#Here we activate the motion of the protein
+#pmb.activate_motion_of_rigid_object(espresso_system=espresso_system,name=protein_name)
 
-pmb.activate_motion_of_rigid_object(espresso_system=espresso_system,name=protein_name)
-
-
-# pmb.center_pmb_object_in_the_simulation_box (name=protein_name,pmb_object_id=0,espresso_system=espresso_system)
+# If we want to center the first protein created
+protein_id = pmb.df.loc[pmb.df['name']==protein_name].molecule_id.values[0]
+pmb.center_molecule_in_simulation_box (molecule_id=protein_id,espresso_system=espresso_system)
 
 pmb.create_counterions_in_espresso (pmb_object='particle',cation_name=cation_name,anion_name=anion_name,espresso_system=espresso_system)
-
 c_salt_calculated = pmb.create_added_salt_in_espresso (espresso_system=espresso_system,cation_name=cation_name,anion_name=anion_name,c_salt=c_salt)
 
 
@@ -115,48 +126,51 @@ RE.set_non_interacting_type (type=non_interacting_type)
 print('The non interacting type is set to ', non_interacting_type)
 
 # Setup the potential energy
-pmb.setup_lj_interactions_in_espresso (espresso_system=espresso_system)
 
-print('\nMinimazing system energy\n')
-espresso_system.cell_system.skin = 0.4
-espresso_system.time_step = dt 
-print('steepest descent')
-espresso_system.integrator.set_steepest_descent(f_max=0, gamma=0.1, max_displacement=0.1)
-espresso_system.integrator.run(1000)
-print('velocity verlet')
-espresso_system.integrator.set_vv()  # to switch back to velocity Verlet
-espresso_system.integrator.run(1000)
-espresso_system.thermostat.turn_off()
-print('\nMinimization finished \n')
+if (WCA):
+    pmb.setup_lj_interactions_in_espresso (espresso_system=espresso_system)
 
-#Electrostatic energy setup 
-print ('Electrostatics setup')
-bjerrum_length = pmb.e.to('reduced_charge')**2 / (4 *pmb.units.pi*pmb.units.eps0* solvent_permitivity * pmb.kT.to('reduced_energy'))
-coulomb_prefactor = bjerrum_length.to('reduced_length') * pmb.kT.to('reduced_energy')
-coulomb = espressomd.electrostatics.P3M ( prefactor = coulomb_prefactor.magnitude, accuracy=1e-3)
+    print('\nMinimazing system energy\n')
+    espresso_system.cell_system.skin = 0.4
+    espresso_system.time_step = dt 
+    print('steepest descent')
+    espresso_system.integrator.set_steepest_descent(f_max=0, gamma=0.1, max_displacement=0.1)
+    espresso_system.integrator.run(1000)
+    print('velocity verlet')
+    espresso_system.integrator.set_vv()  # to switch back to velocity Verlet
+    espresso_system.integrator.run(1000)
+    espresso_system.thermostat.turn_off()
+    print('\nMinimization finished \n')
 
-print('\nBjerrum length ', bjerrum_length.to('nm'), '=', bjerrum_length.to('reduced_length'))
+    if (Electrostatics):
+        #Electrostatic energy setup 
+        print ('Electrostatics setup')
+        bjerrum_length = pmb.e.to('reduced_charge')**2 / (4 *pmb.units.pi*pmb.units.eps0* solvent_permitivity * pmb.kT.to('reduced_energy'))
+        coulomb_prefactor = bjerrum_length.to('reduced_length') * pmb.kT.to('reduced_energy')
+        coulomb = espressomd.electrostatics.P3M ( prefactor = coulomb_prefactor.magnitude, accuracy=1e-3)
 
-espresso_system.time_step = dt
-espresso_system.actors.add(coulomb)
+        print('\nBjerrum length ', bjerrum_length.to('nm'), '=', bjerrum_length.to('reduced_length'))
 
-# save the optimal parameters and add them by hand
-p3m_params = coulomb.get_params()
-espresso_system.actors.remove(coulomb)
+        espresso_system.time_step = dt
+        espresso_system.actors.add(coulomb)
 
-coulomb = espressomd.electrostatics.P3M (
-                            prefactor = coulomb_prefactor.magnitude,
-                            accuracy = 1e-3,
-                            mesh = p3m_params['mesh'],
-                            alpha = p3m_params['alpha'] ,
-                            cao = p3m_params['cao'],
-                            r_cut = p3m_params['r_cut'],
-                            tune = False,
-                                )
+        # save the optimal parameters and add them by hand
+        p3m_params = coulomb.get_params()
+        espresso_system.actors.remove(coulomb)
 
-espresso_system.actors.add(coulomb)
+        coulomb = espressomd.electrostatics.P3M (
+                                    prefactor = coulomb_prefactor.magnitude,
+                                    accuracy = 1e-3,
+                                    mesh = p3m_params['mesh'],
+                                    alpha = p3m_params['alpha'] ,
+                                    cao = p3m_params['cao'],
+                                    r_cut = p3m_params['r_cut'],
+                                    tune = False,
+                                        )
 
-print("\nElectrostatics successfully added to the system \n")
+        espresso_system.actors.add(coulomb)
+
+        print("\nElectrostatics successfully added to the system \n")
 
 #Save the initial state 
 
@@ -181,10 +195,10 @@ net_charge_list = []
 
 net_charge_amino_save = {}
 
-for step in tqdm(range(Samples_per_pH+steps_eq)):
+for step in tqdm(range(N_samples)):
         
         if pmb.np.random.random() > probability_reaction:
-            espresso_system.integrator.run(steps=MD_steps_per_sample)
+            espresso_system.integrator.run(steps=integ_steps)
         else:
             RE.reaction(steps=total_ionisible_groups)
 
@@ -202,7 +216,7 @@ for step in tqdm(range(Samples_per_pH+steps_eq)):
         for amino in net_charge_aminoacids.keys():            
             net_charge_amino_save [amino].append (net_charge_aminoacids[amino])
 
-        if (step % N_samples_print == 0 ):
+        if (step % stride_traj == 0  ):
             n_frame +=1
             pmb.write_output_vtf_file(espresso_system=espresso_system,n_frame=n_frame)
 
@@ -212,26 +226,24 @@ observables_df['Znet'] = net_charge_list
 for amino in net_charge_amino_save.keys():
     observables_df[amino] = net_charge_amino_save[amino]
 
-observables_df.to_csv(f'pH-{pH_value}_observables_.csv',index=False)
-
+observables_df.to_csv(f'pH-{pH_value}_observables.csv',index=False)
 
 print(pmb.df)
 print (observables_df)
 
+# from espressomd import visualization
 
-from espressomd import visualization
-
-espresso_system.time_step=1e-3
-espresso_system.thermostat.set_langevin(kT=1, gamma=1.0, seed=24)
-espresso_system.cell_system.tune_skin(min_skin=0.01, max_skin =4.0, tol=0.1, int_steps=1000)
-visualizer = espressomd.visualization.openGLLive(espresso_system, bond_type_radius=[0.3], background_color=[1, 1, 1],particle_type_colors=[[1.02,0.51,0], # Brown
-                [1,1,1],  # Grey
-                [2.55,0,0], # Red
-                [0,0,2.05],  # Blue
-                [0,0,2.05],  # Blue
-                [2.55,0,0], # Red
-                [2.05,1.02,0]])     
-visualizer.run(1)
+# espresso_system.time_step=1e-3
+# espresso_system.thermostat.set_langevin(kT=1, gamma=1.0, seed=24)
+# espresso_system.cell_system.tune_skin(min_skin=0.01, max_skin =4.0, tol=0.1, int_steps=1000)
+# visualizer = espressomd.visualization.openGLLive(espresso_system, bond_type_radius=[0.3], background_color=[1, 1, 1],particle_type_colors=[[1.02,0.51,0], # Brown
+#                 [1,1,1],  # Grey
+#                 [2.55,0,0], # Red
+#                 [0,0,2.05],  # Blue
+#                 [0,0,2.05],  # Blue
+#                 [2.55,0,0], # Red
+#                 [2.05,1.02,0]])     
+# visualizer.run(1)
 
 # filename = 'protein.png'
 
