c64dd6fd581b369a374c4ed0ede955d34f125d27
Pao <paotorres89@gmail.com>
2024-03-19 20:39:33 -0300

    clean up of the test

--- testsuite/read-write-df_test.py
+++ testsuite/read-write-df_test.py
@@ -1,12 +1,9 @@
 import espressomd
 from espressomd import interactions
-from espressomd.io.writer import vtf
-from espressomd import electrostatics 
 
 import os
 import sys
 import inspect
-import numpy as np
 
 # Find path to pyMBE
 current_dir= os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -30,7 +27,7 @@ path_to_pka=pmb.get_resource("parameters/pka_sets/Hass2015.txt")
 pmb.load_interaction_parameters (filename=path_to_interactions) 
 pmb.load_pka_set (path_to_pka)
 
-#Define the particles 
+#Define particles
 
 pmb.define_particle(
     name = "I",
@@ -65,11 +62,8 @@ pmb.define_residue(
 
 
 # Defines the example molecule of the paper 
-
 molecule_name = "A_molecule"
 n_molecules = 1
-
-molecule_concentration = 5.56e-4 *pmb.units.mol/pmb.units.L
 
 pmb.define_molecule(
     name = molecule_name,
@@ -95,25 +89,6 @@ pmb.define_particle(name=cation_name, q=1, diameter=0.35*pmb.units.nm, epsilon=1
 pmb.define_particle(name=anion_name,  q=-1, diameter=0.35*pmb.units.nm,  epsilon=1*pmb.units('reduced_energy'))
 
 
-# System parameters
-# volume = n_molecules/(pmb.N_A*molecule_concentration)
-# L = volume ** (1./3.) # Side of the simulation box
-# # calculated_molecule_concentration = n_molecules/(volume*pmb.N_A)
-
-# # Create an instance of an espresso system
-# espresso_system=espressomd.System (box_l = [L.to('reduced_length').magnitude]*3)
-
-# # Add all bonds to espresso system
-# pmb.add_bonds_to_espresso(espresso_system=espresso_system)
-
-# # Create your molecules into the espresso system
-# pmb.create_pmb_object(name=molecule_name, number_of_objects= n_molecules,espresso_system=espresso_system, use_default_bond=True)
-
-# pmb.create_counterions(object_name=molecule_name,cation_name=cation_name,anion_name=anion_name,espresso_system=espresso_system) # Create counterions for the peptide chains
-
-# c_salt_calculated = pmb.create_added_salt(espresso_system=espresso_system,cation_name=cation_name,anion_name=anion_name,c_salt=c_salt)
-
-
 #Write the pymbe DF to a csv file 
 
 df_filename = 'df-example_molecule.csv'
@@ -126,17 +101,23 @@ read_df = pmb.read_pmb_df(df_filename)
 
 #Compare both df
 
-#comparison of dtype of each column 
+
 
 dtype_comparison = pmb.df.dtypes == read_df.dtypes 
+
+print(f"*** Unit test: test that the dtype between the written pmb.df and the file read are equal  ***")
 
 if not dtype_comparison.all():
     print (dtype_comparison)
     raise ValueError ('There are two columns that have different dtype.')
 else:
-    print ('All columns have the same format')
+    print(f"*** Unit test passed ***")
 
-#Comparison between each row 
+
+print(f"*** Unit test: test that the rows between the written pmb.df and the file read are equal  ***")
+
 row_comparison = pmb.df.equals(read_df)
 if not row_comparison:
     raise ValueError ('There is some inconsistency between the pmb.df and read_df')
+else:
+    print(f"*** Unit test passed ***")
