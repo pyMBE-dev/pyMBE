86a293731af1f6cc84824c70524b9186df862300
Pao <paotorres89@gmail.com>
2024-04-16 12:59:10 -0300

    adapted standireze_data

--- maintainer/standarize_data.py
+++ maintainer/standarize_data.py
@@ -10,7 +10,10 @@ pmb = pyMBE.pymbe_library()
 # Expected inputs
 supported_filenames=["Glu-HisMSDE.csv",
                      "Lys-AspMSDE.csv",
-                     "histatin5_SoftMatter.txt"]
+                     "histatin5_SoftMatter.txt",
+                     "1beb-10mM-torres.dat",
+                     "1f6s-10mM-torres.dat"]
+
 
 parser = argparse.ArgumentParser(description='Script to standarize the data from various authors')
 parser.add_argument('--src_filename', 
@@ -25,7 +28,9 @@ filename=args.src_filename
 # Outputs
 output_filenames={"Lys-AspMSDE.csv": "Lunkad2021a.csv",
                   "Glu-HisMSDE.csv": "Lunkad2021b.csv",
-                  "histatin5_SoftMatter.txt": "Blanco2020a.csv"}
+                  "histatin5_SoftMatter.txt": "Blanco2020a.csv",
+                  "1beb-10mM-torres.dat": "Torres2017.csv",
+                  "1f6s-10mM-torres.dat": "Torres2022.csv"}
 
 # Sanity checks
 if filename not in supported_filenames:
@@ -35,6 +40,7 @@ if filename not in supported_filenames:
 ref_path=pmb.get_resource(f"testsuite/data/src/{filename}")
 Refs_lunkad=["Glu-HisMSDE.csv","Lys-AspMSDE.csv"]
 Ref_blanco=["histatin5_SoftMatter.txt"]
+Ref_torres = ["1f6s-10mM-torres.dat","1beb-10mM-torres.dat" ]
 
 if filename in Refs_lunkad:
     data=pd.read_csv(ref_path)
@@ -42,15 +48,31 @@ if filename in Refs_lunkad:
     # Error propagation calculation
     # 1/4 factor added to correct for bug in the original calculation of the error reported by the authors       
     Z_ref_err = 5/4*np.sqrt((data['eaa'])**2+(data['eab'])**2)   
+    pH_range = np.linspace(2, 12, num=21)
     
 elif filename in Ref_blanco:
     data=np.loadtxt(ref_path, delimiter=",")
     Z_ref=data[:,1]         
     Z_ref_err=data[:,2]
+    pH_range = np.linspace(2, 12, num=21)
+    
+elif filename in Ref_torres:
+
+    Z_ref = []
+    Z_ref_err = []
+    pH_range = []
+    
+    with open (ref_path,'r') as file:
+        for line in file: 
+            line_split = line.split ()
+            pH = float (line_split[0])
+            pH_range.append(pH)
+            Z_ref.append (float(line_split[1]))
+            Z_ref_err.append(float(line_split[2]))
+
+
 else:
     raise RuntimeError()
-
-pH_range = np.linspace(2, 12, num=21)
 
 # Store the data
 data=pd.DataFrame({"pH": pH_range,
