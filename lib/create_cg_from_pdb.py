7d3bd51e255caaebd081e83e8da2e176189459c8
student <pinedaps@natur.cuni.cz>
2024-06-05 15:53:02 +0200

    Removing title line in the license, adding whitespace between the hashtag and text, removing license from tutorials

--- lib/create_cg_from_pdb.py
+++ lib/create_cg_from_pdb.py
@@ -1,20 +1,20 @@
-#pyMBE : The python-based Molecule Builder for ESPResSo.
-#Copyright (C) 2024 pyMBE-dev team
 #
-#This file is part of pyMBE.
+# Copyright (C) 2024 pyMBE-dev team
 #
-#pyMBE is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+# This file is part of pyMBE.
 #
-#pyMBE is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# pyMBE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# pyMBE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 import argparse
