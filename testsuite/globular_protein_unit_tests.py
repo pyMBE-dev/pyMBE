df78b34f9cdc242f3854fe6bb4c11fc7467b90cf
Paola <paotorres89@gmail.com>
2025-05-13 12:15:47 -0300

    added search_residue test

--- testsuite/globular_protein_unit_tests.py
+++ testsuite/globular_protein_unit_tests.py
@@ -371,7 +371,6 @@ print("*** Unit test: check that define_AA_residues()")
 
 test_sequence = ['c','n', 'G','V', 'I', 'L', 'E', 'Q', 'D', 'N', 'H', 'W', 'F', 'Y', 'R', 'K', 'S', 'T', 'M', 'A', 'G', 'P', 'C' ]
 
-
 valid_protein_model = ['1beadAA', '2beadAA']
 
 output =['AA-c', 'AA-n', 'AA-G', 'AA-V', 'AA-I', 'AA-L', 'AA-E', 'AA-Q', 'AA-D', 'AA-N', 'AA-H', 'AA-W', 'AA-F', 'AA-Y', 'AA-R', 'AA-K', 'AA-S', 'AA-T', 'AA-M', 'AA-A', 'AA-G', 'AA-P', 'AA-C']
@@ -385,28 +384,42 @@ for protein_model in valid_protein_model:
                             desired=output, 
                             verbose=True)
 
-
-
 print("*** Unit test passed ***")
 
 print("*** Unit test: check that define_peptide() raises a ValueError if a wrong model key is provided")
 
-input_parameters = {'name': 'generic_peptide',
-                    'sequence': 'EEEEEEE',
-                    'model': '3beadAA' }
+input_parameters = {"name": "generic_peptide",
+                    "sequence": "EEEEEEE",
+                    "model": "3beadAA" }
 
-np.testing.assert_raises(ValueError, pmb.protein_sequence_parser, **input_parameters)
+np.testing.assert_raises(ValueError, pmb.define_peptide, **input_parameters)
 
+input_parameters = {"name": "generic_peptide",
+                    "sequence": "EEEEEEE",
+                    "model": "beadAA" }
 
-input_parameters = {'name': 'generic_peptide',
-                    'sequence': 'EEEEEEE',
-                    'model': 'beadAA' }
-
-np.testing.assert_raises(ValueError, pmb.protein_sequence_parser, **input_parameters)
+np.testing.assert_raises(ValueError, pmb.define_peptide, **input_parameters)
 
 print("*** Unit test passed ***")
 
+print("*** Unit test: check that search_particles_in_residue() returns the correct list of residues")
 
-print("*** Unit test: check that search_particles_in_residue()")
+list_of_residues = ['AA-c', 'AA-n', 'AA-G', 'AA-V', 'AA-I', 'AA-L', 'AA-E', 'AA-Q', 'AA-D', 'AA-N', 'AA-H', 
+                    'AA-W', 'AA-F', 'AA-Y', 'AA-R', 'AA-K', 'AA-S', 'AA-T', 'AA-M', 'AA-A','AA-P', 'AA-C']
 
+for residue_name in list_of_residues:
+
+    residue = residue_name.replace('AA-','')
+
+    list_of_particles_in_residue= pmb.search_particles_in_residue(residue_name = residue_name)
+
+    if residue in ['c', 'n', 'G']:        
+        np.testing.assert_equal(actual=list_of_particles_in_residue, 
+                                desired=[residue], 
+                                verbose=True)
+    else:
+        np.testing.assert_equal(actual=list_of_particles_in_residue, 
+                                desired=['CA', residue], 
+                                verbose=True)
+                                
 print("*** Unit test passed ***")
