f8299d0542a83e21defe82d42e46a27b2582be4a
David Beyer <dbeyer@icp.uni-stuttgart.de>
2025-05-14 21:43:26 +0200

    add unit test for reservoir concentration determination in interacting case

--- testsuite/determine_reservoir_concentrations_unit_test.py
+++ testsuite/determine_reservoir_concentrations_unit_test.py
@@ -19,8 +19,10 @@
 # Import pyMBE and other libraries
 import pyMBE
 import numpy as np
+import pandas as pd
+from scipy import interpolate
 
-def determine_reservoir_concentrations_test(pH_res, c_salt_res):
+def determine_reservoir_concentrations_test_ideal(pH_res, c_salt_res):
 
     # Create an instance of the pyMBE library
     pmb = pyMBE.pymbe_library(seed=42)
@@ -38,14 +40,51 @@ def determine_reservoir_concentrations_test(pH_res, c_salt_res):
     cNa_res = max(c_salt_res, c_salt_res + cOH_res - cH_res) 
     cCl_res = max(c_salt_res, c_salt_res + cH_res - cOH_res) 
 
-    # Check the determine equilibrium constants
+    # Check the determined concentrations 
     np.testing.assert_allclose(cH_res, cH_res_pyMBE)
     np.testing.assert_allclose(cOH_res, cOH_res_pyMBE)
     np.testing.assert_allclose(cNa_res, cNa_res_pyMBE)
     np.testing.assert_allclose(cCl_res, cCl_res_pyMBE)
 
-print("*** Unit test: check that pyMBE correctly calculates the reservoir composition when setting up the grand-reaction method. ***")
+def determine_reservoir_concentrations_test_interacting(pH_res, c_salt_res, reference_data):
+
+    # Create an instance of the pyMBE library
+    pmb = pyMBE.pymbe_library(seed=42)
+
+    # Load the excess chemical potential data
+    path_to_ex_pot=pmb.get_resource("parameters/salt")
+    monovalent_salt_ref_data=pd.read_csv(f"{path_to_ex_pot}/excess_chemical_potential_excess_pressure.csv")
+    ionic_strength = pmb.units.Quantity(monovalent_salt_ref_data["cs_bulk_[1/sigma^3]"].values, "1/reduced_length**3")
+    excess_chemical_potential = pmb.units.Quantity(monovalent_salt_ref_data["excess_chemical_potential_[kbT]"].values, "reduced_energy")
+    excess_chemical_potential_interpolated = interpolate.interp1d(ionic_strength.m_as("1/reduced_length**3"), 
+                                                                                    excess_chemical_potential.m_as("reduced_energy"))
+    activity_coefficient_monovalent_pair = lambda x: np.exp(excess_chemical_potential_interpolated(x.to('1/(reduced_length**3 * N_A)').magnitude))
+
+    # Determine the reservoir composition using pyMBE
+    cH_res_pyMBE, cOH_res_pyMBE, cNa_res_pyMBE, cCl_res_pyMBE = pmb.determine_reservoir_concentrations(
+            pH_res,
+            c_salt_res * pmb.units.mol/ pmb.units.L,
+            activity_coefficient_monovalent_pair)
+
+    # Check the results
+    row = reference_data[(reference_data['pH_res'] == pH_res) & (reference_data['c_salt_res'] == c_salt_res)]
+    np.testing.assert_allclose(cH_res_pyMBE.magnitude, row["cH_res"])
+    np.testing.assert_allclose(cOH_res_pyMBE.magnitude, row["cOH_res"])
+    np.testing.assert_allclose(cNa_res_pyMBE.magnitude, row["cNa_res"])
+    np.testing.assert_allclose(cCl_res_pyMBE.magnitude, row["cCl_res"])
+
+
+print("*** Unit test: check that pyMBE correctly calculates the reservoir composition when setting up the grand-reaction method (ideal case). ***")
 for pH_res in np.linspace(1.0, 13.0, 5):
     for c_salt_res in np.logspace(-5, 0, 5):
-        determine_reservoir_concentrations_test(pH_res, c_salt_res)
+        determine_reservoir_concentrations_test_ideal(pH_res, c_salt_res)
+print("*** Unit test passed ***")
+
+print("*** Unit test: check that pyMBE correctly calculates the reservoir composition when setting up the grand-reaction method (interacting case). ***")
+# Load reference data
+reference_data = pd.read_csv('./determine_reservoir_concentrations_test_data/data.csv')
+for pH_res in np.linspace(1.0, 13.0, 5):
+    for c_salt_res in np.logspace(-5, -1, 4):
+        print(pH_res, c_salt_res)
+        determine_reservoir_concentrations_test_interacting(pH_res, c_salt_res, reference_data)
 print("*** Unit test passed ***")
