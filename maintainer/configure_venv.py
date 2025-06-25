ac2d179b23e2084bff2164c99597f26103e2ef5b
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2025-06-25 16:15:44 +0200

    Set OpenMP environment variables

--- maintainer/configure_venv.py
+++ maintainer/configure_venv.py
@@ -17,6 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import stat
 import argparse
 import sysconfig
 try:
@@ -34,7 +35,7 @@ def make_pth(name, path):
         f.write(os.path.realpath(path))
 
 
-parser = argparse.ArgumentParser(description="Configure pyBME and ESPResSo module paths")
+parser = argparse.ArgumentParser(description="Configure pyMBE and ESPResSo module paths")
 parser.add_argument("--espresso_path", type=str, required=not espressomd_found,
                     help="Path to the ESPResSo build folder")
 args = parser.parse_args()
@@ -45,3 +46,42 @@ if not os.environ.get("VIRTUAL_ENV"):
 if not espressomd_found:
     make_pth("espresso", os.path.join(args.espresso_path, "src", "python"))
 make_pth("pymbe", os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+def make_venv_set_omp(name, value):
+    return f"""
+_OLD_VIRTUAL_{name}="${name}"
+if test -z "${name}"; then
+  export {name}={value}
+fi"""
+
+
+def make_venv_unset_env(name):
+    return f"""    if [ -n "${{_OLD_VIRTUAL_{name}:-}}" ] ; then
+        {name}="${{_OLD_VIRTUAL_{name}:-}}"
+        export {name}
+        unset _OLD_VIRTUAL_{name}
+    fi"""
+
+
+if os.environ.get("VIRTUAL_ENV"):
+    venv_activate = os.path.join(os.environ.get("VIRTUAL_ENV"), "bin", "activate")
+    if os.path.isfile(venv_activate):
+        original_access_mode = os.stat(venv_activate).st_mode
+        os.chmod(venv_activate, original_access_mode | stat.S_IWUSR)
+        try:
+            with open(venv_activate, "r+") as f:
+                content = f.read()
+                token = "\nexport PATH"
+                assert token in content
+                content = content.replace(token, f"{token}\n{make_venv_set_omp('OMP_PROC_BIND', 'false')}\n{make_venv_set_omp('OMP_NUM_THREADS', '1')}")
+                token = "unset _OLD_VIRTUAL_PATH\n    fi"
+                assert token in content
+                content = content.replace(token, f"{token}\n{make_venv_unset_env('OMP_PROC_BIND')}\n{make_venv_unset_env('OMP_NUM_THREADS')}""")
+                f.seek(0)
+                f.truncate()
+                f.write(content)
+        except:
+            raise
+        finally:
+            os.chmod(venv_activate, original_access_mode)
